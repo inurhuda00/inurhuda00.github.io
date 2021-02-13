bda6ce44319996dddf1c94242998eefcacf51c08
Ilham Nuruddin Al Huda <inurhuda00@gmail.com>
2021-02-13 08:45:14 +0700

    Deploy Next.js to gh-pages

--- next-env.d.ts
+++
@@ -1,5 +0,0 @@
-//  <reference types="next" />
-// <reference types="next/types/global" />
-
-declare module 'mdx-prism'
-declare module '@mapbox/rehype-prism'
--- types/author.ts
+++
@@ -1,7 +0,0 @@
-type Author = {
-    name: string
-    url: string
-    picture: string
-}
-
-export default Author
